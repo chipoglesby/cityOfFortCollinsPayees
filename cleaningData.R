2a91c3007e3e4ece06dc0d60a244d89e8448a742
chipoglesby <chip@creativesparkcolumbia.com>
2016-12-28 07:57:40 -0500

    Adding and cleaning data.

---
+++ cleaningData.R
@@ -0,0 +1,41 @@
+library(rvest)
+library(jsonlite)
+library(gdata)
+library(dplyr)
+
+landingPage <- read_html("http://www.fcgov.com/opendata/")
+
+links <- landingPage %>%
+  html_nodes('a') %>%
+  html_attr('href')
+
+glServiceAreaLink <- "https://s3.amazonaws.com/fortcollins-opendata/glservicearea.json"
+glFundLink <- "https://s3.amazonaws.com/fortcollins-opendata/glfund.json"
+glDepartmentLink <- "https://s3.amazonaws.com/fortcollins-opendata/gldepartment.json"
+glLedgerLinks <- links[grepl("https://s3.amazonaws.com/fortcollins-opendata/generalledger-",links) == TRUE]
+
+data <- fromJSON(glLedgerLinks[1])
+
+for(i in 1:length(glLedgerLinks)){
+  data <- bind_rows(data, fromJSON(glLedgerLinks[i]))
+}
+
+# Lowercase column names
+names(data) <- tolower(names(data))
+
+# Lowercase vendor info
+data$glvendor <- tolower(data$glvendor)
+data$glexplanation <- tolower(data$glexplanation)
+
+# Clean up data types
+data$glfund <- as.factor(data$glfund)
+data$glamount <- as.numeric(data$glamount)
+data$gldeptno <- as.factor(data$gldeptno)
+data$glserviceareaid <- as.factor(data$glserviceareaid)
+data$glpo <- as.factor(data$glpo)
+data$gldate <- as.Date(data$gldate)
+
+# Columns are null and can be dropped
+
+data$glvisadesc <- NULL
+data$glvisacardholder <- NULL
