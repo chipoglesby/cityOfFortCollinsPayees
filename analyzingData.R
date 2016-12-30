4ee5b03c9df3aa7c1978f03165c96062f6e32403
Chip Oglesby <chip@creativesparkcolumbia.com>
2016-12-30 09:35:50 -0700

    Updating plots

--- analyzingData.R
+++ analyzingData.R
@@ -2,18 +2,19 @@ source("cleaningData.R")
 library(ggplot2)
 library(lubridate)
 
-data = fetchAndCleanData()
-data$year = year(data$gldate)
-data$month = month(data$gldate,label=TRUE)
-data$day = day(data$gldate)
-data$dayOfWeek = wday(data$gldate,label=TRUE)
+data <- fetchAndCleanData()
+data$year <-year(data$gldate)
+data$month <- month(data$gldate, label = TRUE)
+data$day <- day(data$gldate)
+data$dayOfWeek <-  wday(data$gldate, label = TRUE)
 
-##What Days of the Month are the Checks Cut?
-p = ggplot(data, aes(day, fill = month))
-p + geom_density(alpha = 0.5)
-#Looks like the end of the month - overwhelmingly
+## What Days of the Month are the Checks Cut?
+dayPlot <- ggplot(data, aes(day, fill = month)) +
+geom_density(alpha = 0.5)
 
-#Who Were the Top Vendors of 2015?
+# Looks like the end of the month - overwhelmingly
+
+# Who Were the Top Vendors of 2015?
 vendorSummary <- data %>% group_by(glvendor) %>%
   filter(year == 2015) %>%
   summarize(
@@ -25,12 +26,33 @@ vendorSummary <- data %>% group_by(glvendor) %>%
 
 vendorTop10 <- top_n(vendorSummary, 10, payments)
 
-p = ggplot(vendorTop10,aes(x=reorder(glvendor,averagePayment),y=averagePayment))
-p + geom_bar(stat='identity') + coord_flip() + labs(title='Average Payments to Vendor')
+averagePaymentPlot <- vendorTop10 %>% 
+  ggplot(aes(reorder(glvendor,averagePayment), averagePayment)) + 
+  geom_bar(stat='identity') + 
+  coord_flip() + 
+  ggtitle("Average Payment Per Vendor") +
+  xlab("Vendor") +
+  ylab("Average Payments") +
+  theme(plot.title = 
+          element_text(
+            family = "Trebuchet MS", 
+            color = "#666666", 
+            face = "bold",
+            size = 22,
+            hjust = 0.5)) +
+  theme(axis.title = 
+          element_text(
+            family = "Trebuchet MS",
+            color = "#666666",
+            face = "bold", 
+            size = 18,
+            hjust = 0.5))
 
 
-#Top Explanations for 2015 + 2015
-explanationSummary <- data %>% group_by(glexplanation) %>%
+# Top Explanations for 2015 + 2015
+explanationSummary <- 
+  data %>% 
+  group_by(glexplanation) %>%
   summarize(
     payments = sum(glamount), 
     count = length(glexplanation),
@@ -41,31 +63,71 @@ explanationSummary <- data %>% group_by(glexplanation) %>%
 
 explanationTop10 <- top_n(explanationSummary, 15, payments)
 
-p = ggplot(explanationTop10,aes(x=reorder(glexplanation,averagePayment),y=averagePayment))
-p + geom_bar(stat='identity') + coord_flip() + labs(title='Average Payments by Explanation')
+explanationTop10Plot <- explanationTop10 %>%
+  ggplot(aes(reorder(glexplanation, averagePayment), averagePayment)) +
+  geom_bar(stat='identity') + 
+  coord_flip() + 
+  ggtitle("Average Payments by Explanation") +
+  xlab("Explanation") +
+  ylab("Average Payments") +
+  theme(plot.title = 
+          element_text(
+            family = "Trebuchet MS", 
+            color = "#666666", 
+            face = "bold",
+            size = 22,
+            hjust = 0.5)) +
+  theme(axis.title = 
+          element_text(
+            family = "Trebuchet MS",
+            color = "#666666",
+            face = "bold", 
+            size = 18,
+            hjust = 0.5))
 
-
-#Top Explanations for 2015 + 2015
-explanationSummary <- data %>% 
-  filter(grepl('energy purchases',glexplanation) == FALSE) %>% 
-  group_by(glexplanation) %>%
+# Top Explanations for 2015 + 2015
+explanationSummary1516 <- data %>% 
+  filter(grepl('energy purchases', glexplanation) == FALSE 
+         & is.na(glexplanation) == FALSE
+         & grepl('2015|2016', year)) %>% 
+  group_by(year, glexplanation) %>%
   summarize(
     payments = sum(glamount), 
     count = length(glexplanation),
     averagePayment = 
       round(sum(glamount)/ length(glexplanation),2)) %>%
-  filter(is.na(glexplanation) == FALSE) %>%
   arrange(desc(averagePayment))
 
 head(explanationSummary,15)
 tail(explanationSummary,25)
 
-explanationTop10 <- top_n(explanationSummary, 50, payments)
+explanationTop10 <- top_n(explanationSummary1516, 50, payments)
 
-p = ggplot(explanationTop10,aes(x=reorder(glexplanation,payments),y=payments))
-p + geom_bar(stat='identity') + coord_flip() + labs(title='Payments by Explanation')
+explanationSummary15Plot <- 
+  explanationTop10 %>%
+  filter(year == 2015) %>% 
+  ggplot(aes(reorder(glexplanation,payments), payments)) +
+  geom_bar(stat='identity') +
+  coord_flip() + 
+  ggtitle("2015 Payments by Explanation") +
+  xlab("Explanation") +
+  ylab("Payments") +
+  theme(plot.title = 
+          element_text(
+            family = "Trebuchet MS", 
+            color = "#666666", 
+            face = "bold",
+            size = 22,
+            hjust = 0.5)) +
+  theme(axis.title = 
+          element_text(
+            family = "Trebuchet MS",
+            color = "#666666",
+            face = "bold", 
+            size = 18,
+            hjust = 0.5))
 
-#how much were 
+# how much were 
 a = data[data$glexplanation=='educational programs',]
 a = a[is.na(a)==FALSE,]
 
