d65c6ecf2194b808de7d40598c00821330e5a1fd
stoltzmaniac <scott@stoltzmanconsulting.com>
2016-12-30 14:30:47 -0700

    Revert "Updating plots"

--- analyzingData.R
+++ analyzingData.R
@@ -2,19 +2,18 @@ source("cleaningData.R")
 library(ggplot2)
 library(lubridate)
 
-data <- fetchAndCleanData()
-data$year <-year(data$gldate)
-data$month <- month(data$gldate, label = TRUE)
-data$day <- day(data$gldate)
-data$dayOfWeek <- wday(data$gldate, label = TRUE)
+data = fetchAndCleanData()
+data$year = year(data$gldate)
+data$month = month(data$gldate,label=TRUE)
+data$day = day(data$gldate)
+data$dayOfWeek = wday(data$gldate,label=TRUE)
 
-## What Days of the Month are the Checks Cut?
-dayPlot <- ggplot(data, aes(day, fill = month)) +
-geom_density(alpha = 0.5)
+##What Days of the Month are the Checks Cut?
+p = ggplot(data, aes(day, fill = month))
+p + geom_density(alpha = 0.5)
+#Looks like the end of the month - overwhelmingly
 
-# Looks like the end of the month - overwhelmingly
-
-# Who Were the Top Vendors of 2015?
+#Who Were the Top Vendors of 2015?
 vendorSummary <- data %>% group_by(glvendor) %>%
   filter(year == 2015) %>%
   summarize(
@@ -26,33 +25,12 @@ vendorSummary <- data %>% group_by(glvendor) %>%
 
 vendorTop10 <- top_n(vendorSummary, 10, payments)
 
-averagePaymentPlot <- vendorTop10 %>% 
-  ggplot(aes(reorder(glvendor,averagePayment), averagePayment)) + 
-  geom_bar(stat='identity') + 
-  coord_flip() + 
-  ggtitle("Average Payment Per Vendor") +
-  xlab("Vendor") +
-  ylab("Average Payments") +
-  theme(plot.title = 
-          element_text(
-            family = "Trebuchet MS", 
-            color = "#666666", 
-            face = "bold",
-            size = 22,
-            hjust = 0.5)) +
-  theme(axis.title = 
-          element_text(
-            family = "Trebuchet MS",
-            color = "#666666",
-            face = "bold", 
-            size = 18,
-            hjust = 0.5))
+p = ggplot(vendorTop10,aes(x=reorder(glvendor,averagePayment),y=averagePayment))
+p + geom_bar(stat='identity') + coord_flip() + labs(title='Average Payments to Vendor')
 
 
-# Top Explanations for 2015 + 2015
-explanationSummary <- 
-  data %>% 
-  group_by(glexplanation) %>%
+#Top Explanations for 2015 + 2015
+explanationSummary <- data %>% group_by(glexplanation) %>%
   summarize(
     payments = sum(glamount), 
     count = length(glexplanation),
@@ -63,71 +41,31 @@ explanationSummary <-
 
 explanationTop10 <- top_n(explanationSummary, 15, payments)
 
-explanationTop10Plot <- explanationTop10 %>%
-  ggplot(aes(reorder(glexplanation, averagePayment), averagePayment)) +
-  geom_bar(stat='identity') + 
-  coord_flip() + 
-  ggtitle("Average Payments by Explanation") +
-  xlab("Explanation") +
-  ylab("Average Payments") +
-  theme(plot.title = 
-          element_text(
-            family = "Trebuchet MS", 
-            color = "#666666", 
-            face = "bold",
-            size = 22,
-            hjust = 0.5)) +
-  theme(axis.title = 
-          element_text(
-            family = "Trebuchet MS",
-            color = "#666666",
-            face = "bold", 
-            size = 18,
-            hjust = 0.5))
+p = ggplot(explanationTop10,aes(x=reorder(glexplanation,averagePayment),y=averagePayment))
+p + geom_bar(stat='identity') + coord_flip() + labs(title='Average Payments by Explanation')
 
-# Top Explanations for 2015 + 2015
-explanationSummary1516 <- data %>% 
-  filter(grepl('energy purchases', glexplanation) == FALSE 
-         & is.na(glexplanation) == FALSE
-         & grepl('2015|2016', year)) %>% 
-  group_by(year, glexplanation) %>%
+
+#Top Explanations for 2015 + 2015
+explanationSummary <- data %>% 
+  filter(grepl('energy purchases',glexplanation) == FALSE) %>% 
+  group_by(glexplanation) %>%
   summarize(
     payments = sum(glamount), 
     count = length(glexplanation),
     averagePayment = 
       round(sum(glamount)/ length(glexplanation),2)) %>%
+  filter(is.na(glexplanation) == FALSE) %>%
   arrange(desc(averagePayment))
 
 head(explanationSummary,15)
 tail(explanationSummary,25)
 
-explanationTop10 <- top_n(explanationSummary1516, 50, payments)
+explanationTop10 <- top_n(explanationSummary, 50, payments)
 
-explanationSummary15Plot <- 
-  explanationTop10 %>%
-  filter(year == 2015) %>% 
-  ggplot(aes(reorder(glexplanation,payments), payments)) +
-  geom_bar(stat='identity') +
-  coord_flip() + 
-  ggtitle("2015 Payments by Explanation") +
-  xlab("Explanation") +
-  ylab("Payments") +
-  theme(plot.title = 
-          element_text(
-            family = "Trebuchet MS", 
-            color = "#666666", 
-            face = "bold",
-            size = 22,
-            hjust = 0.5)) +
-  theme(axis.title = 
-          element_text(
-            family = "Trebuchet MS",
-            color = "#666666",
-            face = "bold", 
-            size = 18,
-            hjust = 0.5))
+p = ggplot(explanationTop10,aes(x=reorder(glexplanation,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip() + labs(title='Payments by Explanation')
 
-# how much were 
+#how much were 
 a = data[data$glexplanation=='educational programs',]
 a = a[is.na(a)==FALSE,]
 
@@ -151,3 +89,58 @@ plot(data$gldate,data$glamount)
 df = data %>% group_by(year,glfund) %>% summarise(payments = sum(glamount))
 p = ggplot(df,aes(x=year,y=payments))
 p + geom_bar(stat='identity') + facet_wrap(~glfund)
+
+
+df = data %>%
+  group_by(SERVICEAREANAME) %>%
+  summarise(payments = sum(glamount))
+p = ggplot(df,aes(x=reorder(SERVICEAREANAME,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip()
+
+
+
+
+df = data %>%
+  group_by(DEPTNAME) %>%
+  summarise(payments = sum(glamount)) %>%
+  arrange(desc(payments)) %>%
+  top_n(50)
+p = ggplot(df,aes(x=reorder(DEPTNAME,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip()
+
+
+df = data %>%
+  group_by(GLFUND) %>%
+  summarise(payments = sum(glamount)) %>%
+  arrange(desc(payments))
+p = ggplot(df,aes(x=reorder(GLFUND,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip()
+
+df = data %>%
+  filter(GLFUND == 'Golf Fund') %>%
+  group_by(glvendor) %>%
+  summarise(payments = sum(glamount)) %>%
+  arrange(desc(payments)) %>%
+  top_n(25)
+p = ggplot(df,aes(x=reorder(glvendor,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip()
+
+df = data %>%
+  filter(GLFUND == 'Keep Fort Collins Great Fund') %>%
+  group_by(glvendor) %>%
+  summarise(payments = sum(glamount)) %>%
+  arrange(desc(payments))
+p = ggplot(df,aes(x=reorder(glvendor,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip()
+
+
+df = data %>%
+  filter(DEPTNAME == 'Patrol') %>%
+  group_by(glvendor) %>%
+  summarise(payments = sum(glamount)) %>%
+  arrange(desc(payments)) %>%
+  top_n(30)
+p = ggplot(df,aes(x=reorder(glvendor,payments),y=payments))
+p + geom_bar(stat='identity') + coord_flip()
+#Scandal with #1 paid vendor = redflex traffic systems
+#http://www.chicagotribune.com/news/watchdog/redlight/ct-red-light-cameras-ceo-guilty-met-20150819-story.html
